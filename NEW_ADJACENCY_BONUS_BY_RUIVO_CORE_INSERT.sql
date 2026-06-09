4b65c2bb9c86c886280dd20b1d40bb1e79eccfa5
hennogous <henno.gous@gmail.com>
2026-06-09 17:19:46 +0200

    Preserve adjacency clone options for unique districts

--- NEW_ADJACENCY_BONUS_BY_RUIVO_CORE_INSERT.sql
+++ NEW_ADJACENCY_BONUS_BY_RUIVO_CORE_INSERT.sql
@@ -6,10 +6,10 @@
 --============================================================================================================================
 --补充给UD： ApplyForUniqueDistricts 参数
     INSERT OR IGNORE INTO Ruivo_New_Adjacency
-    (                                        ID,             DistrictType,     TraitType, ApplyForUniqueDistricts,       NewMethod,       ProvideType,       YieldType,       YieldChange,       AdjacencyType,       CustomAdjacentObject,       Rings,       ModifierOwner,       WhoIsTheOwner,       CollectionType,       Only,       FreeCompose) SELECT 
-    DR.CivUniqueDistrictType || "_" || Ruivo.ID, DR.CivUniqueDistrictType, Dis.TraitType,                       0, Ruivo.NewMethod, Ruivo.ProvideType, Ruivo.YieldType, Ruivo.YieldChange, Ruivo.AdjacencyType, Ruivo.CustomAdjacentObject, Ruivo.Rings, Ruivo.ModifierOwner, Ruivo.WhoIsTheOwner, Ruivo.CollectionType, Ruivo.Only, Ruivo.FreeCompose   
+    (                                        ID,             DistrictType,     TraitType, ApplyForUniqueDistricts,       NewMethod,       ProvideType,       YieldType,       CustomArgumentValue,       YieldChange,       AdjacencyType,       CustomAdjacentObject,       Rings,       MinRings,       MaxRings,       MustOwn,       ModifierOwner,       WhoIsTheOwner,       CollectionType,       Only,       FreeCompose) SELECT
+    DR.CivUniqueDistrictType || "_" || Ruivo.ID, DR.CivUniqueDistrictType, Dis.TraitType,                       0, Ruivo.NewMethod, Ruivo.ProvideType, Ruivo.YieldType, Ruivo.CustomArgumentValue, Ruivo.YieldChange, Ruivo.AdjacencyType, Ruivo.CustomAdjacentObject, Ruivo.Rings, Ruivo.MinRings, Ruivo.MaxRings, Ruivo.MustOwn, Ruivo.ModifierOwner, Ruivo.WhoIsTheOwner, Ruivo.CollectionType, Ruivo.Only, Ruivo.FreeCompose
 
-    FROM Districts AS d                                                     --遍历区域表   
+    FROM Districts AS d                                                     --遍历区域表
     JOIN DistrictReplaces DR ON d.DistrictType = DR.ReplacesDistrictType    --取代区域为对应区域
     JOIN Ruivo_New_Adjacency Ruivo ON d.DistrictType = Ruivo.DistrictType   --将全新系列加成表映射给取代的特色区域
     JOIN Districts Dis ON DR.CivUniqueDistrictType = Dis.DistrictType       --再次回到区域表，取出数值
@@ -22,14 +22,14 @@
 --以下为组装modifier的部分
 --============================================================================================================================
 --自制modifier:给玩家所有区域地基加产--用于提供相邻产出，环数为1环
-    INSERT INTO Types (Type, Kind) VALUES 
+    INSERT INTO Types (Type, Kind) VALUES
     ("RUIVO_MODIFIER_PLAYER_DISTRICTS_ADJUST_BASE_YIELD_CHANGE", "KIND_MODIFIER");
-    INSERT INTO DynamicModifiers (ModifierType, CollectionType, EffectType) VALUES 
+    INSERT INTO DynamicModifiers (ModifierType, CollectionType, EffectType) VALUES
     ("RUIVO_MODIFIER_PLAYER_DISTRICTS_ADJUST_BASE_YIELD_CHANGE", "COLLECTION_PLAYER_DISTRICTS", "EFFECT_ADJUST_DISTRICT_BASE_YIELD_CHANGE");
 --自制modifier:区域自身相邻加成系数
-    INSERT INTO Types (Type, Kind) VALUES 
+    INSERT INTO Types (Type, Kind) VALUES
     ("RUIVO_MODIFIER_PLAYER_DISTRICT_ADJUST_YIELD_MODIFIER", "KIND_MODIFIER");
-    INSERT INTO DynamicModifiers (ModifierType, CollectionType, EffectType) VALUES 
+    INSERT INTO DynamicModifiers (ModifierType, CollectionType, EffectType) VALUES
     ("RUIVO_MODIFIER_PLAYER_DISTRICT_ADJUST_YIELD_MODIFIER", "COLLECTION_OWNER", "EFFECT_ADJUST_DISTRICT_YIELD_MODIFIER");
 --自制modifier:区域获得旅游业绩加成
     INSERT INTO Types (Type, Kind) VALUES
@@ -55,24 +55,24 @@
     SELECT 'RUIVO_MODIFIER_OWNER_CITIE_ADJUST_MODIFIED_FREE_POWER', 'EFFECT_ADJUST_MODIFIED_FREE_POWER_IN_CITY', 'COLLECTION_OWNER_CITY'
     WHERE EXISTS (SELECT 1 FROM Types WHERE Type = 'EFFECT_ADJUST_MODIFIED_FREE_POWER_IN_CITY');
 --自制modifier:为区域自身提供property（只能填整数，是叠加计算的）
-    INSERT INTO Types (Type, Kind) VALUES 
+    INSERT INTO Types (Type, Kind) VALUES
     ("RUIVO_MODIFIER_PLAYER_DISTRICT_ADJUST_PROPERTY", "KIND_MODIFIER");
-    INSERT INTO DynamicModifiers (ModifierType, CollectionType, EffectType) VALUES 
+    INSERT INTO DynamicModifiers (ModifierType, CollectionType, EffectType) VALUES
     ("RUIVO_MODIFIER_PLAYER_DISTRICT_ADJUST_PROPERTY", "COLLECTION_OWNER", "EFFECT_ADJUST_DISTRICT_PROPERTY");
 --自制modifier:为区域所属城市提供property（只能填整数，是叠加计算的）
-    INSERT INTO Types (Type, Kind) VALUES 
+    INSERT INTO Types (Type, Kind) VALUES
     ("RUIVO_MODIFIER_PLAYER_CITY_ADJUST_PROPERTY", "KIND_MODIFIER");
-    INSERT INTO DynamicModifiers (ModifierType, CollectionType, EffectType) VALUES 
+    INSERT INTO DynamicModifiers (ModifierType, CollectionType, EffectType) VALUES
     ("RUIVO_MODIFIER_PLAYER_CITY_ADJUST_PROPERTY", "COLLECTION_OWNER_CITY", "EFFECT_ADJUST_CITY_PROPERTY");
 --自制modifier:为区域所属玩家提供property（只能填整数，是叠加计算的）（因为区域自带所有者的玩家属性）
-    INSERT INTO Types (Type, Kind) VALUES 
+    INSERT INTO Types (Type, Kind) VALUES
     ("RUIVO_MODIFIER_PLAYER_ADJUST_PROPERTY", "KIND_MODIFIER");
-    INSERT INTO DynamicModifiers (ModifierType, CollectionType, EffectType) VALUES 
+    INSERT INTO DynamicModifiers (ModifierType, CollectionType, EffectType) VALUES
     ("RUIVO_MODIFIER_PLAYER_ADJUST_PROPERTY", "COLLECTION_OWNER", "EFFECT_ADJUST_PLAYER_PROPERTY");
 --自制modifier:为全局游戏提供property（只能填整数，是叠加计算的）
-    INSERT INTO Types (Type, Kind) VALUES 
+    INSERT INTO Types (Type, Kind) VALUES
     ("RUIVO_MODIFIER_PLAYER_GAME_ADJUST_PROPERTY", "KIND_MODIFIER");
-    INSERT INTO DynamicModifiers (ModifierType, CollectionType, EffectType) VALUES 
+    INSERT INTO DynamicModifiers (ModifierType, CollectionType, EffectType) VALUES
     ("RUIVO_MODIFIER_PLAYER_GAME_ADJUST_PROPERTY", "COLLECTION_OWNER", "EFFECT_ADJUST_GAME_PROPERTY");
 --============================================================================================================================
 
@@ -88,8 +88,8 @@
     UNION SELECT     ListA.ID || '_' || ListB.Num, 'RUIVO_MODIFIER_OWNER_CITIE_ADJUST_MODIFIED_FREE_POWER',    'REQUIREMENT_' || ListA.ID || '_' || ListB.Num, NULL                                             FROM Ruivo_New_Adjacency ListA JOIN Ruivo_BinaryList ListB ON 1=1 WHERE ListA.FreeCompose = 0 AND ListA.ProvideType = 'SelfPowerModifier'
     UNION SELECT     ListA.ID || '_' || ListB.Num, 'MODIFIER_PLAYER_DISTRICT_ADJUST_DISTRICT_AMENITY',         'REQUIREMENT_' || ListA.ID || '_' || ListB.Num, NULL                                             FROM Ruivo_New_Adjacency ListA JOIN Ruivo_BinaryList ListB ON 1=1 WHERE ListA.FreeCompose = 0 AND ListA.ProvideType = 'SelfAmenity'
     UNION SELECT     ListA.ID || '_' || ListB.Num, 'RUIVO_MODIFIER_PLAYER_DISTRICT_ADJUST_DISTRICT_HOUSING',   'REQUIREMENT_' || ListA.ID || '_' || ListB.Num, NULL                                             FROM Ruivo_New_Adjacency ListA JOIN Ruivo_BinaryList ListB ON 1=1 WHERE ListA.FreeCompose = 0 AND ListA.ProvideType = 'SelfHousing'
-    UNION SELECT     ListA.ID || '_' || ListB.Num, 'RUIVO_MODIFIER_OWNER_CITY_ADJUST_IDENTITY_PER_TURN',       'REQUIREMENT_' || ListA.ID || '_' || ListB.Num, NULL                                             FROM Ruivo_New_Adjacency ListA JOIN Ruivo_BinaryList ListB ON 1=1 WHERE ListA.FreeCompose = 0 AND ListA.ProvideType = 'SelfLoyalty'    
-    UNION SELECT     ListA.ID || '_' || ListB.Num, 'MODIFIER_PLAYER_ADJUST_INFLUENCE_POINTS_PER_TURN',         'REQUIREMENT_' || ListA.ID || '_' || ListB.Num, NULL                                             FROM Ruivo_New_Adjacency ListA JOIN Ruivo_BinaryList ListB ON 1=1 WHERE ListA.FreeCompose = 0 AND ListA.ProvideType = 'SelfInfluence'    
+    UNION SELECT     ListA.ID || '_' || ListB.Num, 'RUIVO_MODIFIER_OWNER_CITY_ADJUST_IDENTITY_PER_TURN',       'REQUIREMENT_' || ListA.ID || '_' || ListB.Num, NULL                                             FROM Ruivo_New_Adjacency ListA JOIN Ruivo_BinaryList ListB ON 1=1 WHERE ListA.FreeCompose = 0 AND ListA.ProvideType = 'SelfLoyalty'
+    UNION SELECT     ListA.ID || '_' || ListB.Num, 'MODIFIER_PLAYER_ADJUST_INFLUENCE_POINTS_PER_TURN',         'REQUIREMENT_' || ListA.ID || '_' || ListB.Num, NULL                                             FROM Ruivo_New_Adjacency ListA JOIN Ruivo_BinaryList ListB ON 1=1 WHERE ListA.FreeCompose = 0 AND ListA.ProvideType = 'SelfInfluence'
     UNION SELECT     ListA.ID || '_' || ListB.Num, 'MODIFIER_PLAYER_ADJUST_EXTRA_FAVOR_PER_TURN',              'REQUIREMENT_' || ListA.ID || '_' || ListB.Num, NULL                                             FROM Ruivo_New_Adjacency ListA JOIN Ruivo_BinaryList ListB ON 1=1 WHERE ListA.FreeCompose = 0 AND ListA.ProvideType = 'SelfFavor'
     UNION SELECT     ListA.ID || '_' || ListB.Num, 'MODIFIER_PLAYER_ADJUST_TRADE_ROUTE_CAPACITY',              'REQUIREMENT_' || ListA.ID || '_' || ListB.Num, NULL                                             FROM Ruivo_New_Adjacency ListA JOIN Ruivo_BinaryList ListB ON 1=1 WHERE ListA.FreeCompose = 0 AND ListA.ProvideType = 'SelfTradeRoute'
     UNION SELECT     ListA.ID || '_' || ListB.Num, 'MODIFIER_PLAYER_ADJUST_GREAT_PERSON_POINTS',               'REQUIREMENT_' || ListA.ID || '_' || ListB.Num, NULL                                             FROM Ruivo_New_Adjacency ListA JOIN Ruivo_BinaryList ListB ON 1=1 WHERE ListA.FreeCompose = 0 AND ListA.ProvideType = 'GreatPersonPoints'
@@ -142,13 +142,13 @@
     SELECT          'REQUIRES_' || ListA.ID || '_' || ListB.Num,       'PropertyName',       ListA.ID || '_' || ListB.Num   FROM Ruivo_New_Adjacency ListA JOIN Ruivo_BinaryList ListB ON 1=1
     UNION SELECT    'REQUIRES_' || ListA.ID || '_' || ListB.Num,       'PropertyMinimum',    1                          	FROM Ruivo_New_Adjacency ListA JOIN Ruivo_BinaryList ListB ON 1=1;
 --req相邻拥有者，环数为1环
-    INSERT INTO RequirementSets (RequirementSetId, RequirementSetType) VALUES 
+    INSERT INTO RequirementSets (RequirementSetId, RequirementSetType) VALUES
     ("REQUIREMENT_RUIVO_ADJACENT_TO_OWNER_DISTRICT", "REQUIREMENTSET_TEST_ALL");
-    INSERT INTO RequirementSetRequirements (RequirementSetId, RequirementId) VALUES 
+    INSERT INTO RequirementSetRequirements (RequirementSetId, RequirementId) VALUES
     ("REQUIREMENT_RUIVO_ADJACENT_TO_OWNER_DISTRICT", "REQUIRES_RUIVO_ADJACENT_TO_OWNER_DISTRICT");
-    INSERT INTO Requirements (RequirementId, RequirementType) VALUES 
+    INSERT INTO Requirements (RequirementId, RequirementType) VALUES
     ("REQUIRES_RUIVO_ADJACENT_TO_OWNER_DISTRICT", "REQUIREMENT_PLOT_ADJACENT_TO_OWNER");
-    INSERT INTO RequirementArguments (RequirementId, Name, Value) VALUES 
+    INSERT INTO RequirementArguments (RequirementId, Name, Value) VALUES
     ("REQUIRES_RUIVO_ADJACENT_TO_OWNER_DISTRICT", "MaxDistance", 1);
 --============================================================================================================================
 
@@ -175,8 +175,8 @@
     AND EXISTS (SELECT 1 FROM Districts WHERE DistrictType = ListA.DistrictType);
 
 --作用对象为玩家所有区域--NewMethod为1
-    INSERT INTO Modifiers         (ModifierId, ModifierType) 
-    SELECT 'ATTACH_' || ListA.ID || '_' || ListB.Num, 'RUIVO_MODIFIER_PLAYER_ALL_DISTRICTS_ATTACH_MODIFIER'   
+    INSERT INTO Modifiers         (ModifierId, ModifierType)
+    SELECT 'ATTACH_' || ListA.ID || '_' || ListB.Num, 'RUIVO_MODIFIER_PLAYER_ALL_DISTRICTS_ATTACH_MODIFIER'
     FROM Ruivo_New_Adjacency ListA JOIN Ruivo_BinaryList ListB ON 1=1
     WHERE ListA.NewMethod = 1
     AND ListA.FreeCompose = 0 AND ListA.ModifierOwner = 'DistrictModifiers'
@@ -184,7 +184,7 @@
 
 --填入数值--NewMethod为1
     INSERT INTO ModifierArguments (ModifierId, Name, Value) SELECT
-    'ATTACH_' || ListA.ID || '_' || ListB.Num, 'ModifierId', ListA.ID || '_' || ListB.Num              
+    'ATTACH_' || ListA.ID || '_' || ListB.Num, 'ModifierId', ListA.ID || '_' || ListB.Num
     FROM Ruivo_New_Adjacency ListA JOIN Ruivo_BinaryList ListB ON 1=1
     WHERE ListA.NewMethod = 1
     AND ListA.FreeCompose = 0 AND ListA.ModifierOwner = 'DistrictModifiers'
@@ -242,7 +242,7 @@
 
 --============================================================================================================================
 --作用目标为：本城所有区域，玩家所有区域
-    INSERT INTO Modifiers         (ModifierId, ModifierType) 
+    INSERT INTO Modifiers         (ModifierId, ModifierType)
     SELECT       'ATTACH_' || ListA.ID || '_' || ListB.Num, 'RUIVO_MODIFIER_OWNER_CITY_DISTRICTS_ATTACH_MODIFIER'   FROM Ruivo_New_Adjacency ListA JOIN Ruivo_BinaryList ListB ON 1=1 WHERE ListA.FreeCompose = 0 AND ListA.ModifierOwner != 'DistrictModifiers' AND ListA.CollectionType = 'COLLECTION_CITY_DISTRICTS'
     UNION SELECT 'ATTACH_' || ListA.ID || '_' || ListB.Num, 'RUIVO_MODIFIER_PLAYER_ALL_DISTRICTS_ATTACH_MODIFIER'   FROM Ruivo_New_Adjacency ListA JOIN Ruivo_BinaryList ListB ON 1=1 WHERE ListA.FreeCompose = 0 AND ListA.ModifierOwner != 'DistrictModifiers' AND ListA.CollectionType = 'COLLECTION_PLAYER_DISTRICTS'
     UNION SELECT 'ATTACH_' || ListA.ID || '_' || ListB.Num, 'RUIVO_MODIFIER_ALL_DISTRICTS_ATTACH_MODIFIER'          FROM Ruivo_New_Adjacency ListA JOIN Ruivo_BinaryList ListB ON 1=1 WHERE ListA.FreeCompose = 0 AND ListA.ModifierOwner != 'DistrictModifiers' AND ListA.CollectionType = 'COLLECTION_ALL_DISTRICTS'
